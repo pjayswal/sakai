f361a4bc70795d6c6da7061576a9200760f53bed
pjayswal <free8ird@gmail.com>
2014-09-19 15:01:28 -0500

    first commit

---
+++ SakaiServices/src/main/java/org/SakaiServices/App.java
@@ -0,0 +1,13 @@
+package org.SakaiServices;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
