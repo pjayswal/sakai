f174fb31879a1cfef735ef37905661ad340d39dc
pjayswal <free8ird@gmail.com>
2014-09-23 14:28:26 -0500

    admin default constructor added

--- SakaiCommons/src/main/java/org/sakai/commons/Admin.java
+++ SakaiCommons/src/main/java/org/sakai/commons/Admin.java
@@ -7,4 +7,7 @@ public class Admin extends Person {
 	public Admin(String name, String phone, String email,Address address,UserCredential user) {
 		super(name, phone, email, address,user);
 	}
+	public Admin() {
+		// TODO Auto-generated constructor stub
+	}
 }
