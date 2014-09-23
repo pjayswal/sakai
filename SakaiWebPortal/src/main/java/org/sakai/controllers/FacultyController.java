b719a1881745f4fefec325dea3c55036a1e0608d
rkkarn32 <rk.karn32@gmail.com>
2014-09-23 01:54:26 -0500

    Faculty Controller updated

--- SakaiWebPortal/src/main/java/org/sakai/controllers/FacultyController.java
+++ SakaiWebPortal/src/main/java/org/sakai/controllers/FacultyController.java
@@ -1,5 +1,9 @@
 package org.sakai.controllers;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
 import org.sakai.commons.Assignment;
 import org.sakai.commons.AssignmentStudent;
 import org.sakai.serviceclients.IFacultyService;
@@ -11,6 +15,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 @RequestMapping(value="/faculty")
@@ -29,36 +35,69 @@ public class FacultyController {
 	@RequestMapping(value="/{id}", method = RequestMethod.GET)
 	public String getSections(Model model, @PathVariable long id){
 		model.addAttribute("sectionList", facultyService.getSecctions(id));
-		return "listSection";
+		return "SectionList";
 	}
 	
-	@RequestMapping(value="/sectionStudent/{id}",method = RequestMethod.GET)
+	@RequestMapping(value="/getStudentList/{id}",method = RequestMethod.GET)
 	public String getStudent(@PathVariable long sectionId , Model model){
-		model.addAttribute("studentList", facultyService.getStudents(sectionId));		
-		return "sectionStudentList";
+		model.addAttribute("studentList", facultyService.getStudents(sectionId));	
+		//model.addAttribute("size", facultyService.getStudents(sectionId).size());	
+		return "StudentList";
 	}
 	
-	@RequestMapping(value="/createAssignment/{id}", method=RequestMethod.POST)
-	public String createAssignment(@PathVariable long id, Assignment assignment, Model model){
+	@RequestMapping(value="createAssignment/{id}", method=RequestMethod.POST)
+	public String createAssignment(@PathVariable long id,@RequestParam("assignmentFile") MultipartFile file, Assignment assignment, Model model){
+		
+		if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+ 
+                // Creating the directory to store file
+                String rootPath = System.getProperty("catalina.home");
+                File dir = new File(rootPath + File.separator + "tmpFiles");
+                if (!dir.exists())
+                    dir.mkdirs();
+ 
+                // Create the file on server
+                File serverFile = new File(dir.getAbsolutePath()
+                        + File.separator + assignment.getAssignments());
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(serverFile));
+                stream.write(bytes);
+                stream.close();
+ 
+//                logger.info("Server File Location="
+//                        + serverFile.getAbsolutePath());
+ 
+//                return "You successfully uploaded file=" + assignment.getAssignments();
+            } catch (Exception e) {
+//                return "You failed to upload " + assignment.getAssignments() + " => " + e.getMessage();
+            }
+        } else {
+//            return "You failed to upload " + assignment.getAssignments()
+//                    + " because the file was empty.";
+        }
+		
 		facultyService.CreateAssignment(id, assignment);
 		model.addAttribute("id", id);
-		return "assignmentList";
+		return "AssignmentList";
 	}
-	@RequestMapping(value="/sectionAssignment/{id}",method=RequestMethod.GET)
+	@RequestMapping(value="/getSectionAssignmentList/{id}",method=RequestMethod.GET)
 	public String getAssignmentList(Model model, @PathVariable long id){
 		model.addAttribute("assignmentList", facultyService.getAssignments(id));
-		return "assignmentList";
+		return "AssignmentList";
 	}
 	
-	@RequestMapping(value="/sectionAssignmentStudent/{id}")
+	@RequestMapping(value="/getAssignmentStudent/{id}",method = RequestMethod.GET)
 	public String getAssignmentStudent(@PathVariable long id, Model model){
-		model.addAttribute("assignmentStudentList", facultyService.getAssignments(id));
-		return "assignmentStudentList";
+		model.addAttribute("assignmentStudentList", facultyService.getAssignmentStudent(id));
+		return "AssignmentStudentList";
 	}
 	
-	@RequestMapping(value="/updateAssignmentStudent/{id}")
+	
+	@RequestMapping(value="/updateAssignmentStudent/{id}", method = RequestMethod.POST)
 	public String updateAssignmentStudent(@PathVariable long id,Model model, AssignmentStudent assignmentStudent){
 		facultyService.UpdateAssignmnetStudent(assignmentStudent);
-		return "assignmentStudentList";
-	}	
+		return "AssignmentStudentList";
+	}		
 }
